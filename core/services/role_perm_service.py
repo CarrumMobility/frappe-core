52aa5ce2d985e38b882260b6bdb9e0459efb1577
Kapil rohilla <kapil.rohilla@carrum.co.in>
2026-05-23 08:31:08 +0530

    fixed

--- core/services/role_perm_service.py
+++ core/services/role_perm_service.py
@@ -40,11 +40,11 @@ CRM_AGENT_PERMISSIONS = {
 
 # Permissions merged on top of ``TEMPLATE_ROLE_NAME`` (union per doctype).
 ADDITIONAL_PERMISSIONS_BY_ROLE: dict[str, dict[str, list[str]]] = {
-	"hub_manager": CRM_AGENT_PERMISSIONS,
+	"Hub Manager": CRM_AGENT_PERMISSIONS,
 	"telecaller": CRM_AGENT_PERMISSIONS,
 	"telecaller_lead": CRM_AGENT_PERMISSIONS,
 	"onboarding": CRM_AGENT_PERMISSIONS,
-	"driver_manager": CRM_AGENT_PERMISSIONS,
+	"Driver Manager": CRM_AGENT_PERMISSIONS,
 }
 
 TEMPLATE_ROLE_NAME = "Sales User"
@@ -58,7 +58,7 @@ _STARTUP_ENQUEUE_DONE = False
 class RolePermService:
 	def __init__(self):
 		self.roles = {
-			"hub_manager": {
+			"Hub Manager": {
 				"role_name": EnumValues.Roles.HUB_MANAGER,
 				"desk_access": 0,
 			},
@@ -74,7 +74,7 @@ class RolePermService:
 				"role_name": EnumValues.Roles.ONBOARDING,
 				"desk_access": 0,
 			},
-			"driver_manager": {
+			"Driver Manager": {
 				"role_name": EnumValues.Roles.DRIVER_MANAGER,
 				"desk_access": 0,
 			},
